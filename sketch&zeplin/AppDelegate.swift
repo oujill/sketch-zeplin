0eb0838dad0532b6e60f61aedd3ff5267dca0781
jillou <jilljill860524@gmail.com>
2020-07-16 19:05:16 +0800

    add storyboard

--- sketch&zeplin/AppDelegate.swift
+++ sketch&zeplin/AppDelegate.swift
@@ -14,6 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let image = UIImage()
+        UINavigationBar.appearance()
         // Override point for customization after application launch.
         return true
     }
---
+++ sketch&zeplin/UIViewController.swift
@@ -0,0 +1,9 @@
+//
+//  UIViewController.swift
+//  sketch&zeplin
+//
+//  Created by JillOU on 2020/7/16.
+//  Copyright © 2020 Jillou. All rights reserved.
+//
+
+import Foundation
